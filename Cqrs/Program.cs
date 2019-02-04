ab1101a3ab488a1c59f2dc383472df2935c8ea83
Spiros <Spyros_Ponaris@hotmail.com>
2019-02-04 19:57:38 +0200

    cqrs

--- Cqrs/Program.cs
+++ Cqrs/Program.cs
@@ -2,11 +2,23 @@
 
 namespace Cqrs
 {
+
+    public class Person
+    {
+        public int Age { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p = new Person
+            {
+                Age = 123
+            };
+
+
+            Console.ReadKey();
         }
     }
 }
